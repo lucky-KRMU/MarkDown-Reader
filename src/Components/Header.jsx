e65b025f535f42e6c6f1b615dc3301722e588c67
lucky-KRMU <2501730142@krmu.edu.in>
2026-07-05 10:35:56 +0000

    add header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+    <header>
+        <h1 className='text-center text-4xl bg-purple-950 py-4 text-white
+        md:text-5xl'>
+        MarkDown Reader</h1>
+    </header>
+    </>
+  )
+}
+
+export default Header
