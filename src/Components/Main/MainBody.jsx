987ea1843e54b049a93c41fb93c2b786996bc6ba
lucky-KRMU <2501730142@krmu.edu.in>
2026-07-05 11:01:57 +0000

    add style and font

--- src/Components/Main/MainBody.jsx
+++ src/Components/Main/MainBody.jsx
@@ -27,12 +27,16 @@ function MainBody() {
 
     return (
         <>
-            <main className='h-[88vh] w-full bg-zinc-800 flex flex-col items-center justify-center'>
+            <main className='h-[88vh] w-full bg-zinc-800 flex flex-col items-center justify-center font-[Commissioner]'>
                 {
                     loader ?
-                        <Markdown>
-                            {fileContent || "None"}
-                        </Markdown>
+                        <div className='text-justify h-[60vh] w-[80vw] 
+                        overflow-y-scroll [scrollbar-width:none] [&::-webkit-scrollbar]:hidden
+                      bg-zinc-600 text-white p-8 rounded-2xl'>
+                            <Markdown>
+                                {fileContent || "None"}
+                            </Markdown>
+                        </div>
                         :
                         <input type="file" name="mdfile" id="mdfile" className='height-[60vh] w-[80vw]  
                         text-center text-white cursor-pointer
