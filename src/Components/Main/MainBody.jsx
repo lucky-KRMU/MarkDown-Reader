e82106c54a9e6e078e29c6e2bf3db08e5833425f
lucky-KRMU <2501730142@krmu.edu.in>
2026-07-05 10:55:40 +0000

    add markdown lib

--- src/Components/Main/MainBody.jsx
+++ src/Components/Main/MainBody.jsx
@@ -1,7 +1,9 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
+import Markdown from 'react-markdown';
 
 function MainBody() {
-    const[fileContent, setFileContent] = useState('');
+    const [loader, setLoader] = useState(false);
+    const [fileContent, setFileContent] = useState('');
 
     const ExtractContent = (e) => {
         const file = e.target.files[0];
@@ -20,18 +22,25 @@ function MainBody() {
         }
 
         reader.readAsText(file);
+        setLoader(true);
     }
 
-  return (
-    <>
-    <main className='h-[88vh] w-full bg-zinc-800 flex items-center justify-center'>
-        <input type="file" name="mdfile" id="mdfile" className='height-[60vh] w-[80vw]  
-        text-center text-white cursor-pointer
-        border-2 border-zinc-950 border-dashed rounded-2xl py-[30vh]' onChange={ExtractContent} />
-        
-    </main>
-    </>
-  )
+    return (
+        <>
+            <main className='h-[88vh] w-full bg-zinc-800 flex flex-col items-center justify-center'>
+                {
+                    loader ?
+                        <Markdown>
+                            {fileContent || "None"}
+                        </Markdown>
+                        :
+                        <input type="file" name="mdfile" id="mdfile" className='height-[60vh] w-[80vw]  
+                        text-center text-white cursor-pointer
+                        border-2 border-zinc-950 border-dashed rounded-2xl py-[30vh]' onChange={ExtractContent} />
+                }
+            </main>
+        </>
+    )
 }
 
 export default MainBody
